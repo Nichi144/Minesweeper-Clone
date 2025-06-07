0eece83253da2314bcf21fe5520a26aa46f86ab5
Nichi144 <ashwin.seetharaman144@gmail.com>
2025-06-07 14:32:17 -0400

    a

---
+++ Assets/Scripts/RemoveSquare.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveSquare : MonoBehaviour
+{
+    public void Delete()
+    {
+        Destroy(gameObject);
+    }
+}
