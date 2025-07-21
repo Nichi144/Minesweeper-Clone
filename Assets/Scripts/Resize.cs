2c3e6d6cba2be4220a5cb0d6a1e7f264b46c5aca
Nichi144 <ashwin.seetharaman144@gmail.com>
2025-07-21 07:45:38 -0400

    Major Update, fixed major bug, added assets and some text
    I managed to fix the issue with the TMP text, looks like the issue was me not using Get Compoenent in Child, then added the logic for the mine and number, added some backround sprites, and added some buttons for quitting the application, resetting the level, and getting back and forth from the menu to the field and visa versa. However, currently only have one Minesweeper mode, need to fix this in the future and resize (and maybe replace mines and other things with better assets.

--- Assets/Scripts/Resize.cs
+++ Assets/Scripts/Resize.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Resize : MonoBehaviour
@@ -14,26 +15,40 @@ public class Resize : MonoBehaviour
 
     private List<GameObject> totallines = new List<GameObject>();
     private List<GameObject> totalboxes = new List<GameObject>();
-    public int Mines;
-    public int Safe;
 
-    private TextMeshProUGUI temp;
-    void Update()
+    [SerializeField] private int Mines;
+    [SerializeField] private int Safe;
+
+    private TMP_Text temp;
+
+    void Start()
     {
-        if (Input.GetKeyDown(KeyCode.O))
+        float shiftX = 0f;
+        if (NumOfXLines % 2 != 0)
         {
-            float shiftX = 0f;
-            if (NumOfXLines % 2 != 0)
-            {
-                shiftX -= 0.5f;
-            }
-            float shiftY = 0f;
-            if (NumOfYLines % 2 != 0)
-            {
-                shiftY -= 0.5f;
-            }
-            GenerateGrid(NumOfXLines, NumOfYLines, shiftX, shiftY);
+            shiftX -= 0.5f;
         }
+        float shiftY = 0.35f;
+        if (NumOfYLines % 2 != 0)
+        {
+            shiftY -= 0.5f;
+        }
+        GenerateGrid(NumOfXLines, NumOfYLines, shiftX, shiftY);
+    }
+
+    public void Reseter()
+    {
+        float shiftX = 0f;
+        if (NumOfXLines % 2 != 0)
+        {
+            shiftX -= 0.5f;
+        }
+        float shiftY = 0f;
+        if (NumOfYLines % 2 != 0)
+        {
+            shiftY -= 0.5f;
+        }
+        GenerateGrid(NumOfXLines, NumOfYLines, shiftX, shiftY);
     }
 
     public void Deleter()
@@ -59,13 +74,15 @@ public class Resize : MonoBehaviour
         Deleter();
 
         Mines = 10;
-        if (y == 36) {
-            
-        } else if ((x > 10) && (y > 10))
+        if (y == 36)
+        {
+
+        }
+        else if ((x > 10) && (y > 10))
         {
             Mines = 40;
-        } 
-        Safe = x*y - Mines;
+        }
+        Safe = x * y - Mines;
 
         // Generate vertical lines
         yLine.transform.localScale = new Vector2(0.05f, 1f * y);
@@ -132,6 +149,8 @@ public class Resize : MonoBehaviour
         }
 
         // Generate Boxes
+        // Blocks are generated in the follwing pattern: start from bottom left, 
+        // go up that collumn until you can't, then move to the bottom block of the next collumn and repeat
         for (int i = 0; i < x; i++)
         {
             for (int j = 0; j < y; ++j)
@@ -139,21 +158,9 @@ public class Resize : MonoBehaviour
                 Vector3 bpos = new Vector3(xStart + k + i - (x / 2), yStart + z + j - (y / 2));
                 GameObject block = PickBlock();
                 totalboxes.Add(GameObject.Instantiate(block, bpos, Quaternion.identity, GameObject.FindGameObjectWithTag("Covers").transform));
-                if (block == mine)  // Blocks are generated in the follwing pattern: start from bottom left, go up that collumn until you can't, then move to the bottom block of the next collumn and repeat
-                {
-                    Debug.Log("Collumn: " + i + " Row: " + (8 - j));
-                }
-                else
-                {
-                    // Issue lies in this section now, seems I can access the correct block but no way currently to access the text message
-
-                    temp = totalboxes[totalboxes.Count - 1].GetComponent<TextMeshProUGUI>();
-                    temp.text = "2";
-
-                    // Destroy(totalboxes[totalboxes.Count - 1]);
-                }
-            }   
+            }
         }
+        DetectMines(x, y);
     }
 
     public GameObject PickBlock()
@@ -166,6 +173,58 @@ public class Resize : MonoBehaviour
         }
         Safe--;
         return safe;
+    }
+
+    public void DetectMines(int x, int y)
+    {
+        List<int> neighbors = new List<int>();
+        
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; ++j) // In each tile
+            {
+                if (totalboxes[i * y + j].name == "MineTile(Clone)") // If it's a mine
+                {
+                    neighbors.Clear();
+                    Debug.Log("Mine - Collumn: " + i + " Row: " + (x - j)); // Print the mine location
+                    if ((i * y + j) % y == y-1) // Top row number
+                    {
+                        neighbors = new List<int> { -y - 1, -y, -1, y - 1, y};
+                    }
+                    else if ((i * y + j) % y == 0) // Bottom row number
+                    {
+                        neighbors = new List<int> { -y, -y + 1, 1, y, y + 1};
+                    }
+                    else // Normal Tile
+                    {
+                        neighbors = new List<int>() { -y - 1, -y, -y + 1, -1, 1, y - 1, y, y + 1 };
+                    }
+
+                    for (int k = 0; k < neighbors.Count; k++) // Then check all neighbors
+                    {
+
+                        if (i * y + j + neighbors[k] >= 0 && i * y + j + neighbors[k] < totalboxes.Count) // If valid
+                        {
+                            if (totalboxes[i * y + j + neighbors[k]].name != "MineTile(Clone)") // And not a mine
+                            {
+                                temp = totalboxes[i * y + j + neighbors[k]].GetComponentInChildren<TMP_Text>();
+                                int cur_text = int.Parse(temp.text) + 1;  // Add one mine score to this neighbor
+                                temp.text = cur_text.ToString();
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
 
